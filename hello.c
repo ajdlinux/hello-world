ab7346af07f5d09acaf3faa19a598b49c48510c1
Andrew Donnellan <andrew.donnellan@au1.ibm.com>
2016-11-18 15:08:45 +1100

    bananas
    Signed-off-by: Andrew Donnellan <andrew.donnellan@au1.ibm.com>

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello world! Maybe on Power for realsies this time!\n");
+	printf("Hello world! Maybe on Power for realsies this time! MAYBE!\n");
 	return 0;
 }
